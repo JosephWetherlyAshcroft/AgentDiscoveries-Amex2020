96f1e4be38b60e95325eeb631f4cc286a63f9536
MMAcode <miroslav.makarov@gmail.com>
2020-10-13 15:56:54 +0100

    before rebasing to master,before merging random branch

--- AgentDiscoveries-Frontend/app/src/components/search-forms/search-location-reports.jsx
+++ AgentDiscoveries-Frontend/app/src/components/search-forms/search-location-reports.jsx
@@ -11,6 +11,9 @@ export default class LocationReportsSearch extends React.Component {
         super(props);
 
         this.state = {
+            locations: [],
+            callSigns: [],
+
             callSign: '',
             locationId: '',
             fromTime: '',
@@ -24,31 +27,47 @@ export default class LocationReportsSearch extends React.Component {
         this.onLocationChange = this.onLocationChange.bind(this);
         this.onFromChange = this.onFromChange.bind(this);
         this.onToChange = this.onToChange.bind(this);
+        this.onLocationChange = this.onLocationChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
-        this.loadNextPage = this.loadNextPage.bind(this);
+    }
+    componentWillMount() {
+        apiGet('locations')
+            .then(results => this.setState({ locations: results }))
+            .catch(() => this.addMessage('Error fetching locations, please try again later', 'danger'));
+        apiGet('agents')
+            .then(results => this.setState({ callSigns: results }))
+            .catch(() => this.addMessage('Error fetching locations, please try again later', 'danger'));
     }
 
     render() {
         return (
             <div className='col-md-8 col-md-offset-2'>
                 <Form onSubmit={this.onSubmit}>
-                    <h3>Search Location Reports XY</h3>
+                    <h3>Search Location Reports</h3>
 
                     <Message message={this.state.message} />
 
                     <FormGroup>
-                        <ControlLabel>Agent Call Sign</ControlLabel>
-                        <FormControl type='text'
-                            placeholder='Enter agent Call Sign'
-                            value={this.state.callSign}
-                            onChange={this.onCallSignChange}/>
+                        <ControlLabel>Call Sign</ControlLabel>
+                        <FormControl componentClass='select' required
+                                     value={this.state.callSign}
+                                     onChange={this.onCallSignChange}
+                                     id='callSign-select'>
+                            <option value='' hidden>Choose a call sign</option>
+                            {this.state.callSigns.map(callSign =>
+                                <option key={callSign.agentId} value={callSign.agentId}>{callSign.callSign}, {callSign.firstName}</option>)}
+                        </FormControl>
                     </FormGroup>
                     <FormGroup>
                         <ControlLabel>Location</ControlLabel>
-                        <FormControl type='number'
-                            placeholder='Enter location ID'
-                            value={this.state.locationId}
-                            onChange={this.onLocationChange}/>
+                        <FormControl componentClass='select' required
+                                     value={this.state.locationId}
+                                     onChange={this.onLocationChange}
+                                     id='location-select'>
+                            <option value='' hidden>Choose a location</option>
+                            {this.state.locations.map(location =>
+                                <option key={location.locationId} value={location.locationId}>{location.location}, {location.siteName}</option>)}
+                        </FormControl>
                     </FormGroup>
                     <FormGroup className='form-inline'>
                         <ControlLabel className='rm-3'>From</ControlLabel>
@@ -62,23 +81,18 @@ export default class LocationReportsSearch extends React.Component {
                             onChange={this.onToChange}/>
                     </FormGroup>
                     <Button type='submit'>Search</Button>
-                    <br/>
-                    <button type="button">Previous lot</button>
-                    {/*<button type="button" onClick={loadNextLot}>Next lot</button>*/}
-                    <button type="submit" onClick={this.loadNextPage}>Next lot</button>
                 </Form>
                 <SearchResult results={this.state.results} />
-
             </div>
         );
     }
 
     onCallSignChange(event) {
-        this.setState({ callSign: event.target.value });
+        this.setState({ callSign: event.target.value && parseInt(event.target.value) });
     }
 
     onLocationChange(event) {
-        this.setState({ locationId: parseInt(event.target.value) });
+        this.setState({ locationId: event.target.value && parseInt(event.target.value) });
     }
 
     onFromChange(event) {
@@ -91,8 +105,6 @@ export default class LocationReportsSearch extends React.Component {
 
     onSubmit(event) {
         event.preventDefault();
-        console.log('ahoj 2');
-
         const params = {
             callSign: this.state.callSign,
             locationId: this.state.locationId,
@@ -104,6 +116,7 @@ export default class LocationReportsSearch extends React.Component {
             .then(results => this.setState({ results: results, message: {} }))
             .catch(error => this.setState({message: {message: error.message, type: 'danger'}}));
     }
+<<<<<<< HEAD
 
     loadNextPage(event){
         event.preventDefault();
@@ -121,4 +134,7 @@ export default class LocationReportsSearch extends React.Component {
             .then(results => this.setState({ results: results, message: {} }))
             .catch(error => this.setState({message: {message: error.message, type: 'danger'}}));
     }
+
+=======
+>>>>>>> parent of 6c9b583... before rebasing to master
 }
