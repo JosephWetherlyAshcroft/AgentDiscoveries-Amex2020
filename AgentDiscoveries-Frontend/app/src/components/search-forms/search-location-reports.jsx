8a4e174c7e1ee6d6478c84710ad5489e956e0fbe
MMAcode <miroslav.makarov@gmail.com>
2020-10-15 00:25:28 +0100

    front end-next and previous buttons

--- AgentDiscoveries-Frontend/app/src/components/search-forms/search-location-reports.jsx
+++ AgentDiscoveries-Frontend/app/src/components/search-forms/search-location-reports.jsx
@@ -20,8 +20,9 @@ export default class LocationReportsSearch extends React.Component {
             toTime: '',
 
             results: [],
-            resultsRangeFrom0: [0,3],
-            miroTest:'miro1',
+            resultsPerPage: 4,
+            resultsRangeFrom0: [0, 3],
+            miroTest: 'miro1',
             message: {}
         };
 
@@ -32,15 +33,17 @@ export default class LocationReportsSearch extends React.Component {
         this.onLocationChange = this.onLocationChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
         this.loadNextPage = this.loadNextPage.bind(this);
+        this.loadPreviousPage = this.loadPreviousPage.bind(this);
+
 
     }
 
     componentWillMount() {
         apiGet('locations')
-            .then(results => this.setState({ locations: results }))
+            .then(results => this.setState({locations: results}))
             .catch(() => this.addMessage('Error fetching locations, please try again later', 'danger'));
         apiGet('agents')
-            .then(results => this.setState({ callSigns: results }))
+            .then(results => this.setState({callSigns: results}))
             .catch(() => this.addMessage('Error fetching locations, please try again later', 'danger'));
     }
 
@@ -50,82 +53,82 @@ export default class LocationReportsSearch extends React.Component {
                 <Form onSubmit={this.onSubmit}>
                     <h3>Search Location Reports</h3>
 
-                    <Message message={this.state.message} />
+                    <Message message={this.state.message}/>
 
                     <FormGroup>
                         <ControlLabel>Call Sign</ControlLabel>
                         <FormControl componentClass='select' required
-                            value={this.state.callSign}
-                            onChange={this.onCallSignChange}
-                            id='callSign-select'>
+                                     value={this.state.callSign}
+                                     onChange={this.onCallSignChange}
+                                     id='callSign-select'>
                             <option value='' hidden>Choose a call sign</option>
                             {this.state.callSigns.map(callSign =>
-                                <option key={callSign.callSign} value={callSign.callSign}>{callSign.callSign}, {callSign.firstName}</option>)}
+                                <option key={callSign.callSign}
+                                        value={callSign.callSign}>{callSign.callSign}, {callSign.firstName}</option>)}
                         </FormControl>
                     </FormGroup>
                     <FormGroup>
                         <ControlLabel>Location</ControlLabel>
                         <FormControl componentClass='select' required
-                            value={this.state.locationId}
-                            onChange={this.onLocationChange}
-                            id='location-select'>
+                                     value={this.state.locationId}
+                                     onChange={this.onLocationChange}
+                                     id='location-select'>
                             <option value='' hidden>Choose a location</option>
                             {this.state.locations.map(location =>
-                                <option key={location.locationId} value={location.locationId}>{location.location}, {location.siteName}</option>)}
+                                <option key={location.locationId}
+                                        value={location.locationId}>{location.location}, {location.siteName}</option>)}
                         </FormControl>
                     </FormGroup>
                     <FormGroup className='form-inline'>
                         <ControlLabel className='rm-3'>From</ControlLabel>
                         <FormControl className='rm-3' type='date'
-                            value={this.state.fromTime}
-                            onChange={this.onFromChange}/>
+                                     value={this.state.fromTime}
+                                     onChange={this.onFromChange}/>
 
                         <ControlLabel className='rm-3'>To</ControlLabel>
                         <FormControl className='rm-3' type='date'
-                            value={this.state.toTime}
-                            onChange={this.onToChange}/>
+                                     value={this.state.toTime}
+                                     onChange={this.onToChange}/>
                     </FormGroup>
                     <Button type='submit'>Search</Button>
-                    <button onClick={this.loadNextPage}>Next {this.state.miroTest}</button>
+                    <button id='nextLotOfResultsButton' onClick={this.loadNextPage}>Next {this.state.miroTest}</button>
+                    <button id='previousLotOfResultsButton' style={{display:'none'}} onClick={this.loadPreviousPage}>Previous {this.state.miroTest}</button>
+                    <button id='previousLotOfResultsButton2' onClick={this.loadPreviousPage}>Previous2 {this.state.miroTest}</button>
                 </Form>
-                <SearchResult results={this.state.results} />
+                <SearchResult results={this.state.results}/>
             </div>
         );
     }
 
     onCallSignChange(event) {
-        this.setState({ callSign: event.target.value});
+        this.setState({callSign: event.target.value});
     }
 
     onLocationChange(event) {
-        this.setState({ locationId: event.target.value && parseInt(event.target.value) });
+        this.setState({locationId: event.target.value && parseInt(event.target.value)});
     }
 
     onFromChange(event) {
-        this.setState({ fromTime: event.target.value });
+        this.setState({fromTime: event.target.value});
     }
 
     onToChange(event) {
-        this.setState({ toTime: event.target.value });
+        this.setState({toTime: event.target.value});
     }
 
     onSubmit(event) {
         event.preventDefault();
-
         //set result range to default values
-
-        const params = {
-            callSign: this.state.callSign,
-            locationId: this.state.locationId,
-            fromTime: this.state.fromTime && moment.utc(this.state.fromTime).startOf('day').toISOString(),
-            toTime: this.state.toTime && moment.utc(this.state.toTime).endOf('day').toISOString(),
-            resultsRange: `${this.state.resultsRangeFrom0[0]}-${this.state.resultsRangeFrom0[1]}`
-
-        };
-        console.log("Miro url params: ",params);
-        const url = 'reports/locationstatuses?' + QueryString.stringify(params);
+        console.log("Miro url params: ", this.getSearchParametersFromState());
+        const url = 'reports/locationstatuses?' + QueryString.stringify(this.getSearchParametersFromState());
         apiGet(url)
-            .then(results => this.setState({results: results, message: {},miroTest:'miro2'}))
+            .then(results => {
+                //check the length of results, if < max, hide "next" button
+                console.log("numberOfResults received from backend: ", results.length);
+                this.hideNextShowPreviousLotButtonIfNeeded();
+                console.log("b");
+                this.setState({results: results, message: {}, miroTest: 'miro2'})
+            })
             .catch(error => this.setState({message: {message: error.message, type: 'danger'}}));
     }
 
@@ -133,17 +136,65 @@ export default class LocationReportsSearch extends React.Component {
         event.preventDefault();
         // console.log(this);
         // this.setState({resultsRangeFrom0:})
-        const params = {
+        this.setState(previousState => ({
+            resultsRangeFrom0: [
+                previousState.resultsRangeFrom0[0] + previousState.resultsPerPage,
+                previousState.resultsRangeFrom0[1] + previousState.resultsPerPage
+            ]
+        }), () => {
+            //to do after state update
+            console.log("Miro url params for 'next': ", this.getSearchParametersFromState());
+            const url = 'reports/locationstatuses?' + QueryString.stringify(this.getSearchParametersFromState());
+            apiGet(url)
+                .then(results => {
+                    let numberOfResults = results.length;
+                    // console.log("numberOfResults received from backend: ", numberOfResults);
+                    this.hideNextShowPreviousLotButtonIfNeeded(results.length);
+                    this.setState({results: results, message: {}})
+                })
+                .catch(error => this.setState({message: {message: error.message, type: 'danger'}}));
+        });
+    }
+
+    loadPreviousPage(event){
+        event.preventDefault();
+        this.setState(previousState => ({
+            resultsRangeFrom0: [
+                previousState.resultsRangeFrom0[0] - previousState.resultsPerPage,
+                previousState.resultsRangeFrom0[1] - previousState.resultsPerPage
+            ]
+        }), () => {
+            //to do after state update
+            if (this.state.resultsRangeFrom0[0]<=0){
+                document.querySelector('#previousLotOfResultsButton').style.display='none';
+            }
+            console.log("Miro url params for 'next': ", this.getSearchParametersFromState());
+            const url = 'reports/locationstatuses?' + QueryString.stringify(this.getSearchParametersFromState());
+            apiGet(url)
+                .then(results => {
+                    let numberOfResults = results.length;
+                    // console.log("numberOfResults received from backend: ", numberOfResults);
+                    document.querySelector('#nextLotOfResultsButton').style.display='default';
+                    this.setState({results: results, message: {}})
+                })
+                .catch(error => this.setState({message: {message: error.message, type: 'danger'}}));
+        });
+    }
+
+    getSearchParametersFromState() {
+        return ({
             callSign: this.state.callSign,
             locationId: this.state.locationId,
             fromTime: this.state.fromTime && moment.utc(this.state.fromTime).startOf('day').toISOString(),
             toTime: this.state.toTime && moment.utc(this.state.toTime).endOf('day').toISOString(),
-            resultsRangeFrom0: `${this.state.resultsRangeFrom0[0]}-${this.state.resultsRangeFrom0[1]}`
-        };
-        const url = 'reports/locationstatuses?' + QueryString.stringify(params);
-        apiGet(url)
-            .then(results => this.setState({results: results, message: {}}))
-            .catch(error => this.setState({message: {message: error.message, type: 'danger'}}));
+            resultsRange: `${this.state.resultsRangeFrom0[0]}-${this.state.resultsRangeFrom0[1]}`
+        });
+    }
+    hideNextShowPreviousLotButtonIfNeeded(resultsLength){
+        if (resultsLength < this.state.resultsPerPage){
+            document.querySelector('#nextLotOfResultsButton').style.display='none';
+        }
+        document.querySelector('#previousLotOfResultsButton').style.display='default';
     }
 
 }
