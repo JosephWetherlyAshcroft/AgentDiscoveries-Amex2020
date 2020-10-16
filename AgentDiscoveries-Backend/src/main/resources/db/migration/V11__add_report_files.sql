3dcdbbdab68409542439c5ecdace1d3982fdbcf8
hayleymoodley <moodley.hayley@gmail.com>
2020-10-16 10:55:07 +0100

    Added migration file of new test reports

---
+++ AgentDiscoveries-Backend/src/main/resources/db/migration/V11__add_report_files.sql
@@ -0,0 +1,30 @@
+INSERT INTO location_reports (report_id, location_id, agent_id, status, report_time, report_body, title)
+VALUES('1', '1','1','1','2020-10-15 13:15:15','test report 1', 'test title 1');
+INSERT INTO location_reports (report_id, location_id, agent_id, status, report_time, report_body, title)
+VALUES('2', '1','1','1','2020-10-15 13:15:15','test report 2', 'test title 2');
+INSERT INTO location_reports (report_id, location_id, agent_id, status, report_time, report_body, title)
+VALUES('3', '1','1','1','2020-10-15 13:15:15','test report 3', 'test title 3');
+INSERT INTO location_reports (report_id, location_id, agent_id, status, report_time, report_body, title)
+VALUES('4', '1','1','1','2020-10-15 13:15:15','test report 4', 'test title 4');
+INSERT INTO location_reports (report_id, location_id, agent_id, status, report_time, report_body, title)
+VALUES('5', '1','1','1','2020-10-15 13:15:15','test report 5', 'test title 5');
+INSERT INTO location_reports (report_id, location_id, agent_id, status, report_time, report_body, title)
+VALUES('6', '1','1','1','2020-10-15 13:15:15','test report 6', 'test title 6');
+INSERT INTO location_reports (report_id, location_id, agent_id, status, report_time, report_body, title)
+VALUES('7', '1','1','1','2020-10-15 13:15:15','test report 7', 'test title 7');
+INSERT INTO location_reports (report_id, location_id, agent_id, status, report_time, report_body, title)
+VALUES('8', '1','1','1','2020-10-15 13:15:15','test report 8', 'test title 8');
+INSERT INTO location_reports (report_id, location_id, agent_id, status, report_time, report_body, title)
+VALUES('9', '1','1','1','2020-10-15 13:15:15','test report 9', 'test title 9');
+INSERT INTO location_reports (report_id, location_id, agent_id, status, report_time, report_body, title)
+VALUES('10', '1','1','1','2020-10-15 13:15:15','test report 10', 'testing the title function 1');
+INSERT INTO location_reports (report_id, location_id, agent_id, status, report_time, report_body, title)
+VALUES('11', '1','1','1','2020-10-15 13:15:15','test report 11', 'testing the title function 2');
+INSERT INTO location_reports (report_id, location_id, agent_id, status, report_time, report_body, title)
+VALUES('12', '1','1','1','2020-10-15 13:15:15','test report 12', 'testing the title function 3');
+INSERT INTO location_reports (report_id, location_id, agent_id, status, report_time, report_body, title)
+VALUES('13', '1','1','1','2020-10-15 13:15:15','test report 13', 'testing the pagination function 1');
+INSERT INTO location_reports (report_id, location_id, agent_id, status, report_time, report_body, title)
+VALUES('14', '1','1','1','2020-10-15 13:15:15','test report 14', 'testing the pagination function 2');
+INSERT INTO location_reports (report_id, location_id, agent_id, status, report_time, report_body, title)
+VALUES('15', '1','1','1','2020-10-15 13:15:15','test report 15', 'testing the pagination function 3');
