b063f6ffbb073de219beb566f19c7eb5e10a3724
MMAcode <miroslav.makarov@gmail.com>
2020-10-16 11:18:07 +0100

    new locations added

---
+++ AgentDiscoveries-Backend/src/main/resources/db/migration/V12__add_test_data_locations.sql
@@ -0,0 +1,6 @@
+INSERT INTO locations (site_name, location, time_zone, region_id) VALUES
+  ('Heart of Europe', 'Prague (Praha)', 'Europe/Prague', 1),
+  ('Agents Party', 'Rio', 'America/Argentina/Rio_Gallegos', 1),
+  ('Holiday resort', 'Tahiti', 'Pacific/Tahiti', 1),
+  ('Avocado farm', 'Mexico', 'America/Mexico_City', 1);
+
